c51ae9c76fc007b79a448eb6eb35e00517c92b70
shadow5551 <yanusikdima@mail.ru>
2017-07-06 02:14:06 +0300

    added roles

---
+++ src/main/java/model/Role.java
@@ -0,0 +1,8 @@
+package model;
+
+
+public enum Role {
+    ADMIN,
+    USER,
+    AGENT
+}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -6,5 +6,54 @@ public class User {
 	private String login;
 	private String password;
 	private String email;
-	
+	private Role role;
+	private int balance;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
 }
