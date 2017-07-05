c3c310074b9143dd04602887075500a7e385ce4a
shadow5551 <yanusikdima@mail.ru>
2017-07-06 02:01:07 +0300

    initial Commit of WorldTour

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import dao.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserController extends HttpServlet {
+    private static final long serialVersionUID = 102831973239L;
+    private UserDao dao;
+
+    public UserController() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
